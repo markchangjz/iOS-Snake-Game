f2c7efb6f9ba4fbe747010e51a4302fb0e43e080
MarkChang <markchang@kkbox.com>
2017-02-14 17:42:28 +0800

    Tunes code.

--- Snake Game/Snake/main.m
+++ Snake Game/Snake/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  Snake
-//
-//  Created by MarkChang on 2017/2/11.
-//  Copyright © 2017年 MarkChang. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
