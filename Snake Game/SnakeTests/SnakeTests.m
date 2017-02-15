2f933410631912abfd06d8ca7683a495f80f6221
MarkChang <shea4a71@gmail.com>
2017-02-15 23:07:16 +0800

    Tunes code.

--- Snake Game/SnakeTests/SnakeTests.m
+++ Snake Game/SnakeTests/SnakeTests.m
@@ -7,13 +7,13 @@
 
 @implementation SnakeTests
 
-- (void)setUp {
+- (void)setUp
+{
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)tearDown
+{
     [super tearDown];
 }
 
